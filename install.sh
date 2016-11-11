bad8fb53b158beeba12e3b6e677256fb2c1b54ab
j <lotigine@me.com>
2016-11-11 16:56:28 +0800

    install.sh 软链接改为复制

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 if [ ! -d "$HOME/.in/lua" ];
 then
     mkdir -p "$HOME/.in"
-    ln -s "$(pwd)/lua" "$HOME/.in/lua"
+    cp -rf "$(pwd)/lua" "$HOME/.in/lua"
 fi
 
 if [ ! -d "$HOME/.in/project" ];
 then
     mkdir -p "$HOME/.in"
-    ln -s "$(pwd)/project" "$HOME/.in/project"
+    cp -rf "$(pwd)/project" "$HOME/.in/project"
 fi
